a26ebc061809acd2db8db109d9cc2d5072e52ffc
Andrey <anyuvo@gmail.com>
2022-06-09 23:01:11 +0300

    install axios, making get request

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,52 +1,23 @@
 import React from 'react';
-import cl from './users.module.css'
+import cl from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../img/user.png';
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                followed: false,
-                fullName: "Dmitry",
-                status: 'I am a boss',
-                location: {city: 'Minsk', country: 'Belarus'},
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Dmitri_Mendeleev.jpg'
-            },
-            {
-                id: 2,
-                followed: true,
-                fullName: "Sasha",
-                status: 'I am a boss too',
-                location: {city: 'Moscow', country: 'Russia'},
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Dmitri_Mendeleev.jpg'
-            },
-            {
-                id: 3,
-                followed: false,
-                fullName: "Andrey",
-                status: 'I am a boss too',
-                location: {city: 'Kiev', country: 'Ukraine'},
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Dmitri_Mendeleev.jpg'
-            },
-            {
-                id: 4,
-                followed: false,
-                fullName: "Oleg",
-                status: 'I am a boss too',
-                location: {city: 'Krasnodar', country: 'Russia'},
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Dmitri_Mendeleev.jpg'
-            },
-        ])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
     }
 
-    return (
-        <div>
-            {
-                props.users.map(u => <div key={u.id}>
+    return <div>
+        {
+            props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={cl.userPhoto} alt='userPhoto'/>
+                            <img src={u.photos.small || userPhoto} className={cl.userPhoto} alt='userPhoto'/>
                         </div>
                         <div>
                             {u.followed
@@ -60,18 +31,17 @@ const Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
-    );
+            </div>)
+        }
+    </div>
 };
 
 export default Users;
