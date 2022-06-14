004c8072337a2cfb11acdc2461f0596c20967b68
Andrey <anyuvo@gmail.com>
2022-06-15 00:39:22 +0300

    hiding users during fetching

--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -38,14 +38,13 @@ class UsersContainer extends React.Component {
 
     render() {
         return <>
-            {this.props.isFetching ? <Preloader/> : null}
-            <Users totalUsersCount={this.props.totalUsersCount}
-                   pageSize={this.props.pageSize}
-                   currentPage={this.props.currentPage}
-                   onPageChanged={this.onPageChanged}
-                   users={this.props.users}
-                   follow={this.props.follow}
-                   unfollow={this.props.unfollow}/>
+            {this.props.isFetching ? <Preloader/> : <Users totalUsersCount={this.props.totalUsersCount}
+                                                           pageSize={this.props.pageSize}
+                                                           currentPage={this.props.currentPage}
+                                                           onPageChanged={this.onPageChanged}
+                                                           users={this.props.users}
+                                                           follow={this.props.follow}
+                                                           unfollow={this.props.unfollow}/>}
         </>
     }
 }
