e9e096051cc04dc536b824b03dec8d88262d2777
Andrey <anyuvo@gmail.com>
2022-06-17 00:15:30 +0300

    baseURL in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,9 @@
 import * as axios from "axios";
 
+const baseURL = 'https://social-network.samuraijs.com/api/1.0/';
+
 export const getUsers = (currentPage = 1, pageSize = 4) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
+    return axios.get(baseURL + `users?page=${currentPage}&count=${pageSize}`,
         {
             withCredentials: true
         })
