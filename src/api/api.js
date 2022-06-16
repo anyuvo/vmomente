57e9f25493fad01df9112d6f18575bfff1cb4ccd
Andrey <anyuvo@gmail.com>
2022-06-17 00:23:30 +0300

    create instance with axios.create in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,15 @@
 import * as axios from "axios";
 
-const baseURL = 'https://social-network.samuraijs.com/api/1.0/';
+let instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "fc8a5ea0-fac7-459b-a425-b54fb229a7b3"
+    }
+});
 
 export const getUsers = (currentPage = 1, pageSize = 4) => {
-    return axios.get(baseURL + `users?page=${currentPage}&count=${pageSize}`,
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`,
         {
             withCredentials: true
         })
