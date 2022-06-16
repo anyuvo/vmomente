323ae25b6d1aa8c40c4621a7292e410e2548f624
Andrey <anyuvo@gmail.com>
2022-06-17 00:29:00 +0300

    rename api.js => usersAPI.js, add usersAPI object

--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -10,14 +10,14 @@ import {
 } from "../../Redux/users-reducer";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
-import {getUsers} from "../../api/api";
+import {usersAPI} from "../../api/usersAPI";
 
 class UsersContainer extends React.Component {
 
     componentDidMount() {
         this.props.toggleIsFetching(true);
 
-        getUsers(this.props.currentPage, this.props.pageSize).then(data => {
+        usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then(data => {
             this.props.toggleIsFetching(false);
             this.props.setUsers(data.items);
             this.props.setTotalUsersCount(data.totalCount);
@@ -28,7 +28,7 @@ class UsersContainer extends React.Component {
         this.props.setCurrentPage(pageNumber);
         this.props.toggleIsFetching(true);
 
-        getUsers(pageNumber, this.props.pageSize).then(data => {
+        usersAPI.getUsers(pageNumber, this.props.pageSize).then(data => {
             this.props.toggleIsFetching(false);
             this.props.setUsers(data.items);
         });
---
+++ src/api/usersAPI.js
@@ -0,0 +1,19 @@
+import * as axios from "axios";
+
+let instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "fc8a5ea0-fac7-459b-a425-b54fb229a7b3"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 4) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,
+            {
+                withCredentials: true
+            })
+            .then(response => response.data)
+    }
+}
