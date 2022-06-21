bb28dfd2a31ef4444a33949df2107bbb8b639b47
Andrey <anyuvo@gmail.com>
2022-06-21 11:39:28 +0300

    create profileAPI.js

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Profile from "./Profile";
-import * as axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../Redux/profile-reducer";
 import {useLocation, useNavigate, useParams} from "react-router-dom";
+import {profileAPI} from "../../api/profileAPI";
 
 class ProfileContainer extends React.Component {
 
@@ -12,8 +12,8 @@ class ProfileContainer extends React.Component {
         if (!userId) {
             userId = 2;
         }
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
-            .then(response => {
+
+        profileAPI.getProfile(userId).then(response => {
                 this.props.setUserProfile(response.data);
             });
     }
