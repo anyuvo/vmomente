d2772853bd64871c4af4ee5f8b45b1f4a30e9b97
Andrey <anyuvo@gmail.com>
2022-06-21 16:40:14 +0300

    take of withRouter to HOC withRouter

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Profile from "./Profile";
 import {connect} from "react-redux";
 import {getUserProfile} from "../../Redux/profile-reducer";
-import {useLocation, useNavigate, useParams} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
+import {withRouter} from "../../hoc/withRouter";
 
 class ProfileContainer extends React.Component {
 
@@ -27,22 +27,6 @@ class ProfileContainer extends React.Component {
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
 })
-
-function withRouter(Component) {
-    function ComponentWithRouterProp(props) {
-        let location = useLocation();
-        let navigate = useNavigate();
-        let params = useParams();
-        return (
-            <Component
-                {...props}
-                router={{location, navigate, params}}
-            />
-        );
-    }
-
-    return ComponentWithRouterProp;
-}
 
 export default compose(
     connect(mapStateToProps, {getUserProfile}),
