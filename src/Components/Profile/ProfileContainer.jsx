cfbe8912eebf58ac8790355b1ee2a426fcc5c66b
Andrey <anyuvo@gmail.com>
2022-06-21 16:28:05 +0300

    import compose from 'redux' in DialogsContainer

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -37,7 +37,7 @@ function withRouter(Component) {
         return (
             <Component
                 {...props}
-                router={{ location, navigate, params }}
+                router={{location, navigate, params}}
             />
         );
     }
