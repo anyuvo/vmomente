d0be1204e68b8c540c5ba2ef236f43f48a8aa2b4
Andrey <anyuvo@gmail.com>
2022-06-27 20:45:58 +0300

    redux-form, login, 75

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import {reduxForm, Field} from 'redux-form';
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={"input"} type="text"/>
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} component={"input"} type="password"/>
+            </div>
+            <div>
+                <Field name={"rememberMe"} component={"input"} type="checkbox"/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+};
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
+    return <>
+        <h1>LOGIN</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </>
+
 };
 
 export default Login;
