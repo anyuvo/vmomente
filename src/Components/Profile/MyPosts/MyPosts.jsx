f6d8a6d603945491a4f1177e665e1cb9c32f29d5
Andrey <anyuvo@gmail.com>
2022-06-28 12:19:57 +0300

    redux-form => formik for MyPosts.jsx

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,32 +1,67 @@
-// import React from 'react';
-// import cl from "./MyPosts.module.css";
-// import Post from "./Post/Post";
+import React from 'react';
+import cl from "./MyPosts.module.css";
+import Post from "./Post/Post";
+import {Field, Form, Formik} from "formik";
 // import {reduxForm, Field} from 'redux-form';
 // import {maxLengthCreator, required} from "../../../utils/validators/validators";
 // import {Textarea} from "../../common/FormsControls/FormsControls";
-//
-// const MyPosts = (props) => {
-//
-//     let postsElements = props.posts.map((element, i) => <Post message={element.message} key={element.id}
-//                                                               likesCount={element.likesCount}/>)
-//
-//     let newPostElement = React.createRef();
-//
-//     let onAddPost = (values) => {
-//         props.addPost(values.newPostText);
-//     }
-//
-//     return (
-//         <div className={cl.postsBlock}>
-//             <h3>My posts</h3>
-//             <AddNewPostFormRedux onSubmit={onAddPost}/>
-//             <div className={cl.posts}>
-//                 {postsElements}
-//             </div>
-//         </div>
-//     );
-// };
-//
+
+const MyPosts = (props) => {
+
+    let postsElements = props.posts.map((element, i) => <Post message={element.message} key={element.id}
+                                                              likesCount={element.likesCount}/>)
+
+    // let newPostElement = React.createRef();
+
+    // let onAddPost = (values) => {
+    //     props.addPost(values.newPostText);
+    // }
+
+    return (
+        <div className={cl.postsBlock}>
+            <h3>My posts</h3>
+            <AddNewPostForm addPost={props.addPost}/>
+            <div className={cl.posts}>
+                {postsElements}
+            </div>
+        </div>
+    );
+};
+
+const AddNewPostForm = (props) => {
+
+    let onAddPost = (values) => {
+        props.addPost(values);
+    }
+
+    return (
+        <Formik
+            initialValues={{
+                newPostText: ""
+            }}
+            onSubmit={(values, {resetForm}) => {
+                onAddPost( values.newPostText );
+                resetForm( {values: ''} );
+            }}
+        >
+            {() => (
+                <Form>
+                    <div>
+                        <Field
+                            name={'newPostText'}
+                            as={'textarea'}
+                            placeholder={'Enter your post text'}
+                        />
+                    </div>
+
+                    <button type={'submit'}>Send</button>
+                </Form>
+            )}
+        </Formik>
+    )
+}
+
+
 // let maxLength10 = maxLengthCreator(10);
 //
 // const AddNewPostForm = (props) => {
@@ -43,5 +78,5 @@
 // }
 //
 // const AddNewPostFormRedux = reduxForm({form: "ProfileAddNewPostForm"})(AddNewPostForm)
-//
-// export default MyPosts;
+
+export default MyPosts;
--- src/Components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/Components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,23 +1,23 @@
-// import React from 'react';
-// import {addPostActionCreator} from "../../../Redux/profile-reducer";
-// import MyPosts from "./MyPosts";
-// import {connect} from "react-redux";
-//
-// let mapStateToProps = (state) => {
-//     return {
-//         posts: state.profilePage.postsData,
-//         newPostText: state.profilePage.newPostText
-//     }
-// }
-//
-// let mapDispatchToProps = (dispatch) => {
-//     return {
-//         addPost: (newPostText) => {
-//             dispatch(addPostActionCreator(newPostText))
-//         }
-//     }
-// }
-//
-// const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
-//
-// export default MyPostsContainer;
+import React from 'react';
+import {addPostActionCreator} from "../../../Redux/profile-reducer";
+import MyPosts from "./MyPosts";
+import {connect} from "react-redux";
+
+let mapStateToProps = (state) => {
+    return {
+        posts: state.profilePage.postsData,
+        newPostText: state.profilePage.newPostText
+    }
+}
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        addPost: (newPostText) => {
+            dispatch(addPostActionCreator(newPostText))
+        }
+    }
+}
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
+
+export default MyPostsContainer;
