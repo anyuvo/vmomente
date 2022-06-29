56c96e25327eae32a00cb0fc620ae646d8df3bd3
Andrey <anyuvo@gmail.com>
2022-06-30 00:42:06 +0300

    create ProfileStatusWithHooks, hook useState

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import cl from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -18,7 +19,7 @@ const ProfileInfo = (props) => {
             </div>
             <div className={cl.descriptionBlock}>
                 <img src={props.profile.photos.large} alt="ava"/>
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
                 <div><b>Полное имя:</b> {props.profile.fullName}</div>
                 <div><b>Обо мне:</b> {props.profile.aboutMe}</div>
             </div>
