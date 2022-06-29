3412e0bf704924ca7dce8fc729d0a0280d1a6f4c
Andrey <anyuvo@gmail.com>
2022-06-29 13:54:40 +0300

    add appReducer, add initialized, show Preloader until initialized, 80

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import cl from "./App.module.css";
 import Navbar from "./Components/Navbar/Navbar";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import DialogsContainer from "./Components/Dialogs/DialogsContainer";
 import UsersContainer from "./Components/Users/UsersContainer";
 import ProfileContainer from "./Components/Profile/ProfileContainer";
 import HeaderContainer from "./Components/Header/HeaderContainer";
 import LoginPage from "./Components/Login/Login";
 import {connect} from "react-redux";
-import {getAuthUserData} from "./Redux/auth-reducer";
 import {withRouter} from "./hoc/withRouter";
 import {compose} from "redux";
+import {initializeApp} from "./Redux/app-reducer";
+import Preloader from "./Components/common/Preloader/Preloader";
 
 class App extends React.Component {
     componentDidMount() {
-        this.props.getAuthUserData();
+        this.props.initializeApp();
     }
 
     render() {
+        if (!this.props.initialized) {
+            return <Preloader />
+        }
         return (
                 <div className={cl.appWrapper}>
                     <HeaderContainer/>
@@ -41,7 +45,11 @@ class App extends React.Component {
     }
 }
 
+const mapStateToProps = (state) => ({
+    initialized: state.app.initialized
+})
+
 export default compose (
     withRouter,
-    connect(null, {getAuthUserData}
+    connect(mapStateToProps, {initializeApp}
 ))(App);
