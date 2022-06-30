ff4f7c1b2372e9b78f544d09536339846627a00d
Andrey <anyuvo@gmail.com>
2022-06-30 23:21:48 +0300

    second test

--- src/Redux/profile-reducer.test.js
+++ src/Redux/profile-reducer.test.js
@@ -1,20 +1,33 @@
 import React from "react";
 import profileReducer, {addPostActionCreator} from "./profile-reducer";
 
+let state = {
+    postsData: [
+        {id: 1, message: "Hi, how are you?", likesCount: 10},
+        {id: 2, message: "I am fine", likesCount: 12}
+    ]
+};
+
 it('length of post should be incremented', () => {
 
     //1. test data
     let action = addPostActionCreator("HELLO");
-    let state = {
-        postsData: [
-            {id: 1, message: "Hi, how are you?", likesCount: 10},
-            {id: 2, message: "I am fine", likesCount: 12}
-        ]
-    };
 
     //2. action
     let newState = profileReducer(state, action);
 
     //3. expectation
     expect(newState.postsData.length).toBe(3);
+});
+
+it('message of new post should be correct', () => {
+
+    //1. test data
+    let action = addPostActionCreator("HELLO");
+
+    //2. action
+    let newState = profileReducer(state, action);
+
+    //3. expectation
+    expect(newState.postsData[2].message).toBe("HELLO");
 });
