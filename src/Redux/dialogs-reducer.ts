8344366d72245f0063acd8771f753b95fa749323
Andrey <anyuvo@gmail.com>
2022-07-12 16:39:47 +0300

    TS typing in dialogs-reducer for reducers and actions

--- src/Redux/dialogs-reducer.ts
+++ src/Redux/dialogs-reducer.ts
@@ -1,19 +1,30 @@
 const ADD_MESSAGE = 'ADD-MESSAGE';
 
+type DialogType = {
+    name: string,
+    id: number
+}
+type MessageType = {
+    id: number,
+    message: string
+}
+
 let initialState = {
     dialogsData: [
         {name: "Andrey", id: 1},
         {name: "Oleg", id: 2},
         {name: "Irina", id: 3}
-    ],
+    ] as Array<DialogType>,
     messagesData: [
         {id: 1, message: "hi"},
         {id: 2, message: "how are you"},
         {id: 3, message: "ok ok"}
-    ]
+    ] as Array<MessageType>
 }
 
-const dialogsReducer = (state = initialState, action) => {
+export type InitialStateType = typeof initialState;
+
+const dialogsReducer = (state = initialState, action: any): InitialStateType => {
 
     switch (action.type) {
         case ADD_MESSAGE:
@@ -30,7 +41,12 @@ const dialogsReducer = (state = initialState, action) => {
     }
 }
 
-export const sendMessageCreator = (newMessageBody) => {
+type SendMessageCreatorActionType = {
+    type: typeof ADD_MESSAGE,
+    newMessageBody: string
+}
+
+export const sendMessageCreator = (newMessageBody: string): SendMessageCreatorActionType => {
     return {
         type: ADD_MESSAGE,
         newMessageBody
