17bbe8762cb41b63046b16ed87e47b0e5785c4bc
Andrey <anyuvo@gmail.com>
2022-07-12 18:38:37 +0300

    TS typing in users-reducer for reducers and actions. Create types.ts

--- src/Redux/profile-reducer.ts
+++ src/Redux/profile-reducer.ts
@@ -1,37 +1,10 @@
 import {profileAPI} from "../api/profileAPI";
+import {PostType, ProfileType} from "../types/types";
 
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
 const DELETE_POST = 'DELETE_POST';
-
-type PostType = {
-    id: number,
-    message: string,
-    likesCount: number
-}
-type ContactsType = {
-    github: string,
-    vk: string,
-    facebook: string,
-    instagram: string,
-    twitter: string,
-    website: string,
-    youtube: string,
-    mainLink: string
-}
-type PhotosType = {
-    small: string | null,
-    large: string | null
-}
-type ProfileType = {
-    userId: number,
-    lookingForAJob: boolean,
-    lookingForAJobDescription: string,
-    fullName: string,
-    contacts: ContactsType,
-    photos: PhotosType
-}
 
 let initialState = {
     postsData: [
